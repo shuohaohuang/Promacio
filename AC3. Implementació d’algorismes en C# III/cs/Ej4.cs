fa17e4c9f76d11c7b84b73b9bab2360263deaa17
shuohaohuang <148758354+shuohaohuang@users.noreply.github.com>
2023-11-13 17:35:13 +0100

    Add files via upload

---
+++ AC3. Implementació d’algorismes en C# III/cs/Ej4.cs
@@ -0,0 +1,49 @@
+﻿/*
+* Author: Shuohao Huang
+* M03. Programació UF1
+* 4. Implementa un programa que generi 
+* un array, indicant la seva mida per 
+* teclat. L’usuari haurà d’indicar 
+* un número per teclat, de manera que 
+* els valors de cada posició seran 
+* nombres múltiples consecutius del valor
+* introduït per teclat. Per exemple, si 
+* es defineix un array de grandària 5 i 
+* s’indica un 3 com a valor, l’array 
+* contindrà els valors 3, 6, 9, 12, 15.
+*
+*/
+using System;
+
+namespace Ej4
+{
+    class Ej4
+    {
+        static void Main()
+        {
+            const string Msg = "introdueix la grandaria de la lista";
+            const string MsgValue = "introdueix el primer valor de la lista";
+            const int One = 1;
+            Console.WriteLine(Msg);
+            int[] userArray= new int[Convert.ToInt32(Console.ReadLine())];
+            Console.WriteLine(MsgValue);
+            
+            int value= Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < userArray.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    userArray[i] = value;
+                }
+                else
+                {
+                    userArray[i] = ( i + One)*value;
+                }
+
+                Console.Write(userArray[i]+" ");
+            }
+        }
+    }
+}
---
+++ AC3. Implementació d’algorismes en C# III/cs/Ej5.cs
@@ -0,0 +1,39 @@
+﻿
+/*
+* Author: Shuohao Huang
+* M03. Programació UF1
+* 5.Implementa un programa que generi 
+* una matriu de 4x4 i mostri la diagonal 
+* principal (pots inicialitzar-la amb els 
+* valors enters que consideris).
+*
+*/
+using System;
+
+namespace Ej5
+{
+    class Ej5
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4,4];
+            
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = i * j;
+                    if (j == matrix.GetLength(1) - 1) 
+                    {
+                        Console.WriteLine(i==j? matrix[i, j] : " ");
+
+                    }
+                    else
+                    {
+                        Console.Write(i == j ? matrix[i, j] : " ");
+                    }
+                }
+            }
+        }
+    }
+}
