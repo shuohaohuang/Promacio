653fcfa677f5a2fb3688cd35ced564f763328759
shuohaohuang <148758354+shuohaohuang@users.noreply.github.com>
2023-11-20 20:25:16 +0100

    Add files via upload

---
+++ Export/Hanged.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hanged
+{
+    class Hanged
+    {
+        static void Main()
+        {
+            const string Msg = "\t\t**********************************************\n" +
+                               "\t\t************Bienvenid@ al AHORCADO************\n" +
+                               "\t\t**********************************************";
+            const string Selector = "\tEscoje la dificultat:" +
+                                "\tA: Fácil\n" +
+                                "\tB: Medio\n" +
+                                "\tC: Difícil\n" +
+                                "\tD: Experto\n";
+            const string Selected = "Has escogido el nivel {0}\n";
+            const string GetWord = "Introduce unas palabras para continuar";
+
+            string[,] hanged = { {"===================="}, 
+                                 { }, 
+                                 { }, 
+                                 { }, 
+                                 { }, 
+                                 { }, 
+                                 { }, 
+                                 { }, 
+                                 { },
+                                 { }};
+
+            Console.WriteLine(Msg);
+        }
+    }
+}
